3b9b86b064210f7193a3bfbb528b25453d729c0f
Cody Rigney <codyrigney92@gmail.com>
2023-08-08 00:36:10 -0400

    Center modal.

--- custom-plan.js
+++ custom-plan.js
@@ -86,7 +86,7 @@ function Modal(props) {
       aria-labelledby=${labelId}
       aria-hidden="true"
     >
-      <div class="modal-dialog">
+      <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
           <div class="modal-header">
             <h1 class="modal-title fs-5" id=${labelId}>${title}</h1>
